9d78b090f7cbe8a10d0461667485e52cd4cdaf40
Milan Rasic <rasic_m@live.com>
2015-10-17 14:27:32 +0200

    prvi commit

---
+++ Hakaton/src/B2CBot.java
@@ -0,0 +1,65 @@
+
+public class B2CBot {
+
+}
+package org.mozzartbet.hackathon.bots.borntocode;
+
+import java.util.List;
+import java.util.Set;
+
+import org.mozzartbet.hackathon.Card;
+import org.mozzartbet.hackathon.Player;
+import org.mozzartbet.hackathon.actions.Action;
+import org.mozzartbet.hackathon.bots.Bot;
+
+public class B2CBot implements Bot {
+
+	@Override
+	public void joinedTable(int bigBlind) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void handStarted(Player dealer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actorRotated(Player actor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playerUpdated(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void boardUpdated(List<Card> cards, int bet, int pot) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playerActed(Player player) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Action act(int minBet, int currentBet, Set<Action> allowedActions, int currentAmount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
